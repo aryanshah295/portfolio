dea77c2f34cdb498e795ea875ec3ad5445d43bc9
Aryan Shah <aryanshah295@gmail.com>
2025-07-08 20:21:36 -0400

    completed the about-me section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import { Ovo } from "next/font/google";
-import { assets, infoList } from '@/assets/assets';
+import { assets, infoList, toolsData } from '@/assets/assets';
 
 // Initialize it (yes, again)
 const ovo = Ovo({
@@ -22,19 +22,27 @@ const About = () => {
             <p className={`mb-10 max-w-2xl text-neutral-800 dark:text-neutral-200 leading-relaxed ${ovo.className}`}>
                 Hi, I’m <span className="font-semibold text-black dark:text-white">Aryan Shah</span> — a passionate <span className="font-medium text-neutral-700 dark:text-neutral-300">full-stack developer</span> and Master’s student in Computer Science at <span className="font-semibold">Northeastern University</span> (GPA: <span className="font-medium">4.0</span>). I specialize in building <span className="font-medium italic">scalable web applications</span>, <span className="font-medium italic">cloud-native systems</span>, and <span className="font-medium italic">intelligent interfaces</span> that solve real-world problems.<br /><br />
 
-                With hands-on experience in technologies like <span className="font-medium">React</span>, <span className="font-medium">FastAPI</span>, <span className="font-medium">Node.js</span>, <span className="font-medium">Spring Boot</span>, <span className="font-medium">Docker</span>, <span className="font-medium">Kubernetes</span>, and <span className="font-medium">AWS</span>, I enjoy turning complex problems into clean, performant code. During my internship at <span className="font-semibold">RK Websoft Technologies</span>, I worked on real-time data workflows and image processing pipelines — optimizing performance and improving system accuracy to <span className="font-semibold">95%</span>.<br /><br />
-
                 From building cross-platform mobile apps to deploying microservices with CI/CD pipelines and real-time monitoring, I thrive at the intersection of development, architecture, and user experience.<br /><br />
 
                 When I’m not coding, I enjoy exploring emerging tech, refining my design eye, and discovering new coffee spots.
             </p>
 
-            <ul>
+            <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl '>
                 {infoList.map(({icon, iconDark, title, description},index)=> (
-                    <li key={index}>
-                        <Image src={icon} alt={title}/>
-                        <h3>{title}</h3>
-                        <p>{description}</p>
+                    <li className='group border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:[background-color:#803b46] hover:-translate-y-1 duration-500 hover:shadow hover:shadow-black' key={index}>
+                        <Image src={icon} alt={title} className='w-7 mt-3 group-hover:hidden'/>
+                        <Image src={iconDark} alt={title} className="w-7 mt-3 hidden group-hover:block" />
+                        <h3 className='my-4 font-semibold text-gray-700 group-hover:text-white'>{title}</h3>
+                        <p className='text-gray-600 text-sm group-hover:text-white'>{description}</p>
+                    </li>
+                ))}
+            </ul>
+
+            <h4 className={`my-6 text-gray-700 ${ovo.className}`} >Tools: </h4>
+            <ul className='flex items-center gap-3 sm:gap-5'>
+                {toolsData.map((tool, index)=>(
+                    <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-poiinter hover:-translate-y-1 duration-500' key={index}>
+                        <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
                     </li>
                 ))}
             </ul>
